adb75a15edf4baafefdbcf60334043e342069029
Dexter Haslem <dexter.haslem@gmail.com>
2016-11-05 12:47:15 -0600

    stuff

---
+++ asm/statusbr.asm
@@ -0,0 +1,32 @@
+; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+    Do_Status PROTO :DWORD
+
+    .data?
+        hStatus dd ?
+
+    .code
+
+; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+Do_Status proc hParent:DWORD
+
+    LOCAL sbParts[ 2] :DWORD
+
+    invoke CreateStatusWindow,WS_CHILD or WS_VISIBLE or \
+                              SBS_SIZEGRIP,NULL, hParent, 200
+    mov hStatus, eax
+      
+  ; -------------------------------------
+  ; sbParts is a DWORD array of 2 members
+  ; -------------------------------------
+    mov [sbParts +  0], 240
+    mov [sbParts +  4], -1
+
+    invoke SendMessage,hStatus,SB_SETPARTS, 2,ADDR sbParts
+
+    ret
+
+Do_Status endp
+
+; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
